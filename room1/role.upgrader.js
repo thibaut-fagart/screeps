db1cc0f01e76095200f7c1bb904a3f9968e0350d
thibaut.fagart@gmail.com <thibaut.fagart@gmail.com>
2016-06-24 18:37:05 +0200

    upgrader

--- room1/role.upgrader.js
+++ room1/role.upgrader.js
@@ -2,15 +2,22 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity) {
+	    if(creep.carry.energy == 0 || creep.memory.goal =='fill') {
+	        creep.memory.goal='fill';
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
+            }
+            if (creep.carry.energy == creep.carryCapacity) {
+                creep.memory.action='upgrade';
             }
         }
         else {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
+            }
+            if (creep.carry.energy == 0) {
+                creep.memory.action = 'fill';
             }
         }
 	}
